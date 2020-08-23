191ce936dfe46e40980a87551ac83f71a9dd350b
BryanMendoza1997 <bryan.mendoza2015@uteq.edu.ec>
2020-08-23 04:29:26 -0500

    login terminado

--- app/src/main/java/com/example/appgallinas/Login.java
+++ app/src/main/java/com/example/appgallinas/Login.java
@@ -2,13 +2,80 @@ package com.example.appgallinas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.example.appgallinas.WebServices.Asynchtask;
+import com.example.appgallinas.WebServices.WebService;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Login extends AppCompatActivity implements Asynchtask {
+
+    private EditText usuario;
+    private  EditText contrasenia;
+    private ProgressDialog progreso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        usuario=(EditText)findViewById(R.id.txtcorreo);
+        contrasenia=(EditText)findViewById(R.id.txtcontrasenia);
+    }
+    public void iniciar(View view){
+        progreso=new ProgressDialog(this);
+        progreso.setMessage("Verificando...");
+        progreso.show();
+        Map<String, String> datos = new HashMap<String, String>();
+        datos.put("correo",usuario.getText().toString().trim());
+        datos.put("clave",contrasenia.getText().toString().trim());
+        WebService ws= new WebService("https://fotos-quito-liliana-zambrano.000webhostapp.com/login.php",
+                datos, Login.this, Login.this);
+        ws.execute("POST");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        boolean bandera=false;
+        List<String> lista= new ArrayList<>();
+        JSONObject obj = new JSONObject(result);
+        bandera=obj.getBoolean("success");
+        progreso.hide();
+         if(bandera) {
+
+             lista.add(String.valueOf(obj.getInt("idusuario")));
+             lista.add(obj.getString("nombre").toString());
+             lista.add(obj.getString("ROL").toString());
+
+           if (lista.get(2).toString().trim().equals("Cliente")) {
+
+          Intent in = new Intent(this, Cliente.class);
+          startActivity(in);
+
+        } else {
+            Intent in = new Intent(this, Vendedor.class);
+           startActivity(in);
+          }
+        }
+        else {
+          Toast.makeText(this,"Credenciales incorrectas",Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
     }
 }
